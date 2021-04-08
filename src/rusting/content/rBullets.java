06b720679997d814eb72579a8dff1eab4c70a8f6
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2021-04-08 12:08:51 +1000

    pleaseworkpleaseworkpleaseworkpleaseworkpleasework

---
+++ src/rusting/content/rBullets.java
@@ -0,0 +1,32 @@
+package rBullets;
+
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.util.*;
+import mindustry.ctype.*;
+import mindustry.entities.*;
+import mindustry.entities.bullet.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.io.*;
+import mindustry.world.*;
+
+import static mindustry.Vars.*;
+
+public class rustingBullets implements contentList{
+	public static BulletType	
+	//basic bullet
+	basicBulletT
+
+	@Override
+	public void load(){
+	
+	   basicBulletT = new BasicBulletType(1, 5, "shell"){
+            width = 8
+            height = 6
+            
+        }
+
+	}
+}
--- src/rusting/endlessRusting.java
+++ src/rusting/endlessRusting.java
@@ -6,13 +6,16 @@ public class Rusting extends Mod{
 
     @Override
     public void init(){
+        private contentLists = new Seq(rBullets())
     }
-
+    
     @Override
     public void loadContent(){
         @Overide
         public void loadContent(){
-            
+            contentLists.forEach(e -> {
+                e.load()
+            })
         }
     }
 }
